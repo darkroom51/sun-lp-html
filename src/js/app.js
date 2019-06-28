e8719bb2f7491f2ecec9d96740d325f8172589b4
darkroom51 <darkroom@wp.pl>
2019-06-29 01:25:21 +0200

    add services styling

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // variables
     var menuToggleButton = document.querySelector('.navbar__hamburger');
-    var menuUlElement = document.querySelector('.navbar__menu');
+    var menuElement = document.querySelector('.navbar__menu');
 
     // toggle mobile menu
     menuToggleButton.addEventListener('click', function () {
       menuToggleButton.classList.toggle('navbar__hamburger--close');
-      menuUlElement.classList.toggle('navbar__menu--opened');
+      menuElement.classList.toggle('navbar__menu--opened');
     });
     // close mobile menu when link clicked
-    menuUlElement.addEventListener('click', function () {
-      menuUlElement.classList.remove('navbar__menu--opened');
+    menuElement.addEventListener('click', function () {
+      menuElement.classList.remove('navbar__menu--opened');
       menuToggleButton.classList.remove('navbar__hamburger--close');
     });
   
