a522dcbe177492bab268702e93e48219d37ba3b6
darkroom51 <darkroom@wp.pl>
2019-06-28 23:44:14 +0200

    add mobile menu logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,20 @@
 import '../css/main.scss';
 
+document.addEventListener('DOMContentLoaded', function () {
+
+    // variables
+    var menuToggleButton = document.querySelector('.navbar__hamburger');
+    var menuUlElement = document.querySelector('.navbar__menu');
+
+    // toggle mobile menu
+    menuToggleButton.addEventListener('click', function () {
+      menuToggleButton.classList.toggle('navbar__hamburger--close');
+      menuUlElement.classList.toggle('navbar__menu--opened');
+    });
+    // close mobile menu when link clicked
+    menuUlElement.addEventListener('click', function () {
+      menuUlElement.classList.remove('navbar__menu--opened');
+      menuToggleButton.classList.remove('navbar__hamburger--close');
+    });
+  
+  });
